22c2005c370acd9f7ffa8c5533a6921e330fe8fe
Reis <aglemar.reis@gmail.com>
2023-04-06 11:25:16 -0300

    Nada funciona!

---
+++ Atividade 02/src/DateInfo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class DateInfo {
+    public static void main(String[] args) throws Exception {
+        
+        Date hooraAtual = new Date();
+        System.out.println(hooraAtual);
+
+
+        
+    }
+
+}
---
+++ Atividade 02/src/InfoSistema.java
@@ -0,0 +1,17 @@
+public class InfoSistema {
+    public static void main(String[] args) throws Exception {
+        // 1 - CHAMAR A CLASSE DATEINFO, E DAR UM GET EM DATE RETORNANDO DATA
+
+        
+        DateInfo hour = new DateInfo();
+        System.out.println(hour);
+
+        
+
+        // 2 - CHAMAR A CLASSE LANGUAGE, DAR GET E RETORNAR O IDIOMA DO SISTEMA
+        // 3 - CHAMAR A CLASSE RESOLUTION, DAR GET E RETORNAR A RESOLUCAO DO SISTEMA 
+              
+    }
+
+    
+}
